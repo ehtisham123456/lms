d8158bbf9962472aa016a0377a3eb4e1d08d7df9
ehtisham123456 <infoehtishamm@gmail.com>
2017-05-14 13:30:01 +0300

    Form
    added login form

--- login.php
+++ login.php
@@ -1,1 +1,5 @@
-<title>login</title>
+	 <form> 
+  	 		First name:<input type="text" name="name"><br>
+  	 		Last name: <input type="password" name="password"><br>
+  	 				   <input type="submit" value="Submit">
+	 </form>
