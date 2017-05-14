59e6d1148b0aa4ed9604f1a6ba1bd226c26a7fa4
ehtisham123456 <infoehtishamm@gmail.com>
2017-05-14 15:39:34 +0300

    include header and footer to login.php
    include header and footer to login.php

--- login.php
+++ login.php
@@ -1,10 +1,11 @@
+
 <?php include 'header.php';?>
 
 	 <form class="login">
-	 		<label class="first-name">First name:</label>
-  	 		<input class="name-input" type="text" name="name"><br>
-			<label class="Last-name">Last name:</label>
-  	 		<input class="name-input2" type="password" name="password"><br>
-  	 		<input class="button" type="submit" value="Submit">
+	 		<label>First name:</label>
+  	 		<input type="text" name="name"><br>
+			<label>Last name:</label>
+  	 		<input type="password" name="password"><br>
+  	 		<input type="submit" value="Submit">
 	 </form>
 <?php include 'footer.php';?>
