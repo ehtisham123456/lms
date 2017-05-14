59b08adf9ab15cfabcc4dde61488f4cbfe6c1949
ehtisham123456 <infoehtishamm@gmail.com>
2017-05-14 14:31:40 +0300

    added file header.php and footer.php for styling
    I added  files header and footer

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
-	 <form> 
-	 		<label>Firstname:</label>
-  	 		<input type="text" name="name"><br>
-			<label>Last name:</label>
-  	 		<input type="password" name="password"><br>
-  	 		<input type="submit" value="Submit">
+<link rel="stylesheet" href="asset/css/style.css" />
+	 <form class="login">
+	 		<label class="first-name">First name:</label>
+  	 		<input class="name-input" type="text" name="name"><br>
+			<label class="Last-name">Last name:</label>
+  	 		<input class="name-input2" type="password" name="password"><br>
+  	 		<input class="button" type="submit" value="Submit">
 	 </form>
