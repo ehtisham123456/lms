504de350cdf2095f3352ae4231c7ccfe496b599a
ehtisham123456 <infoehtishamm@gmail.com>
2017-05-15 15:16:19 +0500

    Email Password
    i added the email and password attribute in css.php

--- login.php
+++ login.php
@@ -2,8 +2,8 @@
 <?php include 'header.php';?>
 	
 	 <form class="login">
-  	 		<input class="input" type="text" placeholder="Username"  /><br />
-			<input class="input" type="password" placeholder="password"  /><br />
+  	 		<input class="input" type="text" placeholder="Username" name="st_email"  /><br />
+			<input class="input" type="password" placeholder="password" name="st-password"  /><br />
   	 		<input  class="button" type="submit" value="Submit">
 	 </form>
 	 <h1 class="heading">Login</h1>
