65e797ecda88a69fec46f9887255fc3746e56f37
ehtisham123456 <infoehtishamm@gmail.com>
2017-05-15 13:58:37 +0500

    Login Page
    I styled the login page and added some div and other html elements into
    the login page

--- login.php
+++ login.php
@@ -1,11 +1,10 @@
 
 <?php include 'header.php';?>
-
+	
 	 <form class="login">
-  	 		<input type="text" placeholder="Username"  />
-			<input type="password" placeholder="password"  />
-  	 		<input type="submit" value="Submit">
-			<input class="chexk-box" type="checkbox" />
-			<h4 style="margin-top:-19px; margin-left: 27px;">keep me sign in</h4>
+  	 		<input class="input" type="text" placeholder="Username"  /><br />
+			<input class="input" type="password" placeholder="password"  /><br />
+  	 		<input  class="button" type="submit" value="Submit">
 	 </form>
+	 <h1 class="heading">Login</h1>
 <?php include 'footer.php';?>			
