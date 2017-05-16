85f283b7722d4d290e6161fd49ae88ab13815583
ehtisham123456 <infoehtishamm@gmail.com>
2017-05-16 15:47:49 +0500

    extra code
    remove the extra code from file

--- login.php
+++ login.php
@@ -1,17 +1,19 @@
 
 <?php include 'header.php';?>
-	//<?php print_r($_REQUEST); ?>
  		<?php $st_email = $_REQUEST['st_email'];
  		 	  $st_password = $_REQUEST['st_password'];
+
 			  
-			  echo $st_email;
-			  echo $st_password;
+			   if (isset($st_email)) {
+               echo $st_email;
+  			   echo $st_password;
+    		   }
 			  
 			  ?>
 	 		<form class="login" method="post" action="login.php" >
   	 			<input class="input" type="text" placeholder="Username" name="st_email"  /><br />
 				<input class="input" type="password" placeholder="password" name="st_password"  /><br />
-  	 			<input  class="button" type="submit" value="Submit">
+  	 			<input  class="button" type="submit" value="Submit" name="click_on_sumit">
 	 		</form>
 	 	<h1 class="heading">Login</h1>
 <?php include 'footer.php';?>			
