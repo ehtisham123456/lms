dc40203c142886e536816376a4d8544f451aec00
ehtisham123456 <infoehtishamm@gmail.com>
2017-05-16 13:46:49 +0500

    email password
    i added the email password request in login.php

--- login.php
+++ login.php
@@ -1,10 +1,17 @@
 
 <?php include 'header.php';?>
-	
-	 <form class="login" method="post" action="login.php" >
-  	 		<input class="input" type="text" placeholder="Username" name="st_email"  /><br />
-			<input class="input" type="password" placeholder="password" name="st-password"  /><br />
-  	 		<input  class="button" type="submit" value="Submit">
-	 </form>
-	 <h1 class="heading">Login</h1>
+	<?php print_r($_REQUEST); ?>
+ 		<?php $st_email = $_REQUEST['st_email'];
+ 		 	  $st_password = $_REQUEST['st_password'];
+			  
+			  echo "$st_email";
+			  echo "$st_password";
+			  
+			  ?>
+	 		<form class="login" method="post" action="login.php" >
+  	 			<input class="input" type="text" placeholder="Username" name="st_email"  /><br />
+				<input class="input" type="password" placeholder="password" name="st_password"  /><br />
+  	 			<input  class="button" type="submit" value="Submit">
+	 		</form>
+	 	<h1 class="heading">Login</h1>
 <?php include 'footer.php';?>			
