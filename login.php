dd377bf77fb359d36860401692e87e7de907dd9d
ehtisham123456 <infoehtishamm@gmail.com>
2017-05-16 16:56:07 +0500

    code
    i set the code html & css in login.php

--- login.php
+++ login.php
@@ -1,19 +1,24 @@
 
 <?php include 'header.php';?>
- 		<?php $st_email = $_REQUEST['st_email'];
- 		 	  $st_password = $_REQUEST['st_password'];
+ 		<?php 
 
 			  
-			   if (isset($st_email)) {
-               echo $st_email;
-  			   echo $st_password;
-    		   }
-			   ?>  
-			   <h1 class="heading">Login</h1>
-	 		<form class="login" method="post" action="login.php" >
-  	 			<input class="input" type="text" placeholder="Username" name="st_email"  /><br />
-				<input class="input" type="password" placeholder="password" name="st_password"  /><br />
-  	 			<input  class="button" type="submit" value="Submit" name="click_on_sumit">
-	 		</form>
+			   if (isset($_REQUEST['st_email'])) {
+ 				$st_email = $_REQUEST['st_email'];
+ 				$st_password = $_REQUEST['st_password'];
+ 				echo $st_email;
+ 				echo $st_password;
+				}
+		?>  
+			 <!--BEGIN CONTAINER -->
+			 <div class="login-contentainer">
+			  <h1>Login</h1>
+			  <form method="post" action="login.php" >
+			   <input type="text" placeholder="Username" name="st_email"  /><br />
+			   <input type="password" placeholder="password" name="st_password"  /><br />
+			   <input type="submit" value="Submit">
+			  </form><!-- end form -->
+			 <div>
+ 			<!--END CONTAINER-->
 	 	
 <?php include 'footer.php';?>			
