53e19ae9f06c0195921fcbeec323480e17e8caed
ehtisham123456 <infoehtishamm@gmail.com>
2017-05-18 15:42:51 +0500

    login.php
    i connect the login.php with database

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 
 <?php include 'header.php';?>
  		<?php 
-
 			  
 			   if (isset($_REQUEST['st_email'])) {
  				$st_email = $_REQUEST['st_email'];
@@ -9,7 +8,35 @@
  				echo $st_email;
  				echo $st_password;
 				}
-		?>  
+				// Create connection
+				$dbhost = 'localhost';
+   				$dbuser = 'root';
+   				$dbpass = '';
+				$dbname = "db_lms";
+  	 			$conn = mysql_connect($dbhost,$dbuser,$dbpass);
+   				// Check connection
+   				if(! $conn ) {
+      			die('Could not connect: ' . mysql_error());
+				}
+				$sql = "SELECT ,st_email,st_password FROM lms_student";
+				$result = "mysqli_query($conn, $sql)";
+	
+				if (mysqli_num_rows($result) > 0) { 
+				// output data of each row
+				while($row = mysqli_fetch_assoc ($result)) {
+				echo "id: " . $row[""]. " - Name: " . $row["st_email"]. " " . $row["st_password"]. "<br>";
+				}
+				} else {
+				echo "0 results";
+					}
+	
+				'mysqli_close ($conn)';
+				?> 
+
+   
+				
+				
+			
 			 <!--BEGIN CONTAINER -->
 			 <div class="login-contentainer">
 			  <h1>Login</h1>
