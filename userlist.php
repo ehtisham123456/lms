aa7347dec10d3dbc0c587e6a90a25fcd3e69a6ba
ehtisham123456 <infoehtishamm@gmail.com>
2017-05-23 17:07:19 +0500

    userlist
    i addes userlist.php for users list

--- userlist.php
+++ userlist.php
@@ -1,14 +1,36 @@
-	<?php
+				<?php include 'header.php';?>
+  				<?php 
+     
+				//if (isset($_REQUEST['st_email'])) {
+				//$st_email = $_REQUEST['st_email'];
+				//$st_password = $_REQUEST['st_password'];
+				//$st_password = md5($st_password);
 				
-				
-				
-				if (mysqli_num_rows($result) > 30) { 
+				// Create connection
+				$dbhost = 'localhost';
+				$dbuser = 'root';
+				$dbpass = '';
+				$dbname = "db_lms";
+				$conn = mysqli_connect($dbhost,$dbuser,$dbpass);
+				mysqli_select_db($conn, $dbname);
+				// Check connection
+				if(! $conn ) {
+				die('Could not connect: ' . mysqli_error());
+				}
+				$sql = "SELECT * FROM `lms_student`";
+				$result = mysqli_query($conn, $sql);
+			    
+				if (mysqli_num_rows($result) > 0) { 
+				// output data of each row
 				while($row = mysqli_fetch_assoc ($result)) {
-				echo  " - Name: " . $row["st_name"] . $row["st_email"]. " " . $row["st_password"]. "<br>";
+				echo  "Student Name : " . $row["st_name"]. " -- Student Email " . $row["st_email"]. "<br>";
 				}
 				} else {
-				echo "5000 results";
-					}
+				echo "0 results";
+				 }
 				
-				
-	?>
+				mysqli_close ($conn);
+				//}
+				?> 
+			   
+			<?php include 'footer.php';?>
